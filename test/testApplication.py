7b0cd7b52e4563d6a8ef5a8974782955eea093d6
drew <dr5801@cs.ship.edu>
2018-01-04 10:41:59 -0500

    TestHello test is passing

--- test/testApplication.py
+++ test/testApplication.py
@@ -7,4 +7,4 @@ class TestHello(unittest.TestCase):
 
     def testHell0(self):
         response = requests.get(base_url)
-        self.assertEqual({'Hello':'World!'}, response.json())
+        self.assertEqual('Hello World!', response.json())
