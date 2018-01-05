a184ec969b00700311ccbd4a6069279f1064b42d
drew <dr5801@cs.ship.edu>
2018-01-05 13:43:54 -0500

    tried to refactor using db in a database model.py file

--- src/urlshortener/application.py
+++ src/urlshortener/application.py
@@ -1,39 +1,19 @@
 import os
-from contextlib import contextmanager
-
 import flask
 import urlshortener.url
 import sqlalchemy
 import sqlalchemy.ext.declarative
 import sqlalchemy.orm
+import urlshortener.database
 
 page_blueprint = flask.Blueprint('page_blueprint', __name__)
-engine = sqlalchemy.create_engine(os.environ['DATABASE_URL'])
-Base = sqlalchemy.ext.declarative.declarative_base(bind=engine)
-Session = sqlalchemy.orm.sessionmaker(bind=engine)
-
-@contextmanager
-def session_scope():
-    session = sqlalchemy.orm.scoped_session(Session)
-
-    try:
-        yield session
-        session.commit()
-    except:
-        raise
 
 def createApp():
     app = flask.Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     app.register_blueprint(page_blueprint)
+    urlshortener.database.db.init_app(app)
     return app
-
-
-class Pair(Base):
-    __tablename__ = 'pair'
-    id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.Sequence('id'), primary_key=True)
-    short = sqlalchemy.Column(sqlalchemy.String, unique=True)
-    long = sqlalchemy.Column(sqlalchemy.String)
 
 
 @page_blueprint.route('/')
@@ -47,37 +27,33 @@ def add():
 
     for attempt in range(attempts):
 
-        pair = Pair(
+        pair = urlshortener.database.Pair(
             short=urlshortener.url.create_short(),
             long=flask.request.headers['long'],
         )
         pair.long = urlshortener.url.getURL(url=pair.long)
-        with session_scope() as session:
-            session.add(pair)
+        urlshortener.database.db.add(pair)
 
-        break;
-        # try:
-        #     # session.commit()
-        # except sqlalchemy.exc.IntegrityError:
-        #     if attempt < attempts:
-        #         continue
-        #
-        #     raise
-        #
-        # break
+        try:
+            urlshortener.database.db.commit()
+        except sqlalchemy.exc.IntegrityError:
+            if attempt < attempts:
+                continue
+
+            raise
+
+        break
 
     return flask.jsonify(short=pair.short)
 
 
 @page_blueprint.route('/get/<short>')
 def get(short):
-    with session_scope() as session:
-        pair = session.query(Pair).filter_by(short=short).first()
-        return flask.jsonify(long=pair.long)
+    pair = urlshortener.database.Pair.query.filter_by(short=short).first()
+    return flask.jsonify(long=pair.long)
 
 
 @page_blueprint.route('/<short>')
 def redirect(short):
-    with session_scope() as session:
-        pair = Pair(session.query(Pair).filter_by(short=short).first())
-        return flask.redirect(long=pair.long)
+    pair = urlshortener.database.Pair.query.filter_by(short=short).first()
+    return flask.redirect(pair.long)
---
+++ src/urlshortener/database.py
@@ -0,0 +1,9 @@
+import flask_sqlalchemy
+
+db = flask_sqlalchemy.SQLAlchemy()
+
+
+class Pair(db.Model):
+    id = db.Column(db.Integer, primary_keys=True)
+    short = db.Column(db.String, unique=True)
+    long = db.Column(db.String)
