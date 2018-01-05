7d1d38a0a15514accadb7b743a7c87b9ea587a2c
drew <dr5801@cs.ship.edu>
2018-01-05 13:47:32 -0500

    primary_key .... not primary_keys

--- src/urlshortener/database.py
+++ src/urlshortener/database.py
@@ -4,6 +4,6 @@ db = flask_sqlalchemy.SQLAlchemy()
 
 
 class Pair(db.Model):
-    id = db.Column(db.Integer, primary_keys=True)
+    id = db.Column(db.Integer, primary_key=True)
     short = db.Column(db.String, unique=True)
     long = db.Column(db.String)
