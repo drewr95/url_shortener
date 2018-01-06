58af78ceda40fdf365a202ff20607b71f81f2cea
drew <dr5801@cs.ship.edu>
2018-01-06 11:30:41 -0500

    fixed mutual top-level import problem

--- src/urlshortener/application.py
+++ src/urlshortener/application.py
@@ -1,11 +1,6 @@
 import os
 import flask
 import urlshortener.url
-import sqlalchemy
-import sqlalchemy.ext.declarative
-import sqlalchemy.orm
-import urlshortener.database_model
-import urlshortener.url
 
 app = flask.Flask(__name__)
 
@@ -14,6 +9,4 @@ def createApp():
     global app
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     app.register_blueprint(urlshortener.url.page_blueprint)
-    engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    urlshortener.database_model.db_session = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker(bind=engine))
     return app
